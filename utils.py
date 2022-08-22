b459bbdaac85d7b767e6191abc046e3fb88d0a7f
thewestlabs <111796612+thewestlabs@users.noreply.github.com>
2022-08-22 12:56:10 -0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,30 @@
+from machine import Pin, ADC, Timer, UART
+from Device import UART_BLE_PORT, UART_BLE_RX, UART_BLE_TX, ADC_VOLTAGE_SENSOR_PIN, VOLTAGE_MULTIPLIER, GPIO_RELAY_PIN
+
+adc = ADC(ADC_VOLTAGE_SENSOR_PIN)
+uart = UART(UART_BLE_PORT, 9600, rx=Pin(UART_BLE_RX), tx=Pin(UART_BLE_TX))
+conversion_factor = (3.3/65536) * VOLTAGE_MULTIPLIER
+relay = Pin(GPIO_RELAY_PIN, Pin.OUT)
+
+relay_status = 0
+
+def read_voltage() -> float:
+    voltage = adc.read_u16() * conversion_factor
+    return float("{:.2f}".format(voltage))
+  
+def execute_timer(callback, delay):
+    Timer().init(mode=Timer.ONE_SHOT, period=int(delay * 1000), callback=lambda t: callback())
+
+def read_bluetooth():
+    if uart.any():
+        command: bytearray = uart.readline()
+        if command != b'\x00':
+            print(f"bluetooth read: {command} {command.decode()}")
+            return command.decode()
+
+def write_bluetooth(data):
+    uart.write(str(data) + "\n")
+
+def toggle_relay():
+  relay_status = 1 if (self.status == 0) else 0
+  relay.value(relay_status)
