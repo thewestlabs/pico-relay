3a4ebe48478ce711432552a280e20f66ce90fd2f
thewestlabs <111796612+thewestlabs@users.noreply.github.com>
2022-08-22 21:23:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
-from utils import read_voltage, toggle_relay
+from utils import read_voltage, disconnect_relay
 
 while True:
-  time.sleep(2)
-  voltage = read_voltage()
-  print(f"voltage: {voltage}v")
-  if (voltage > 14.4)
-    print("voltage above")
-    disconnect_relay()
+    time.sleep(2)
+    voltage = read_voltage()
+    print(f"voltage: {voltage}v")
+    if (voltage > 14.4):
+        print("voltage above")
+        disconnect_relay()
