9deedeae2e77f0087893181c19dbc03fa4b57f51
thewestlabs <111796612+thewestlabs@users.noreply.github.com>
2022-08-23 22:12:44 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,19 @@
+import re
 from machine import Pin, ADC, Timer, UART
-from Device import UART_BLE_PORT, UART_BLE_RX, UART_BLE_TX, ADC_VOLTAGE_SENSOR_PIN, VOLTAGE_MULTIPLIER, GPIO_RELAY_PIN
+from Device import UART_BLE_PORT, UART_BLE_RX, UART_BLE_TX, ADC_VOLTAGE_SENSOR_PIN, VOLTAGE_MULTIPLIER, GPIO_RELAY_PIN,  I2C_RTC_PORT, I2C_RTC_SCL, I2C_RTC_SDA
 
 adc = ADC(ADC_VOLTAGE_SENSOR_PIN)
 uart = UART(UART_BLE_PORT, 9600, rx=Pin(UART_BLE_RX), tx=Pin(UART_BLE_TX))
 conversion_factor = (3.3/65536) * VOLTAGE_MULTIPLIER
 relay = Pin(GPIO_RELAY_PIN, Pin.OUT)
+rtc = ds3231(I2C_RTC_PORT, I2C_RTC_SCL, I2C_RTC_SDA)
+rtc_i2c_addr = [hex(ii) for ii in rtc.bus.scan()] 
+date_regex = re.compile(" |:|/")
 
 def read_voltage() -> float:
     voltage = adc.read_u16() * conversion_factor
     return float("{:.2f}".format(voltage))
-  
+
 def execute_timer(callback, delay):
     Timer().init(mode=Timer.ONE_SHOT, period=int(delay * 1000), callback=lambda t: callback())
 
@@ -28,3 +32,11 @@ def connect_relay():
 
 def disconnect_relay():
     relay.value(1)
+
+def read_time(self):
+    if rtc_i2c_addr != []:
+        date_time = rtc.read_time()
+        items = date_regex.split(date_time)
+        return [int(items[0]), int(items[1]), int(items[2]), int(items[3]), int(items[4]), int(items[5])]
+    else:
+        return None
