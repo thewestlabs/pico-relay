dd02a95b105fbf080bc71c0db001f639bb410ae3
thewestlabs <111796612+thewestlabs@users.noreply.github.com>
2022-08-24 17:11:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import time
-from utils import read_voltage, disconnect_relay, connect_relay
+from utils import read_voltage, disconnect_relay, connect_relay, read_time
 
 while True:
     time.sleep(2)
     voltage = read_voltage()
-    print(f"voltage: {voltage}v")
-    if (voltage > 14.4):
-        print("voltage high")
-        disconnect_relay()
-    if (voltage > 12.1):
-        print("voltage low")
+    year, month, date, hour, minute, second = read_time()
+
+    time = "{}:{}{}".format(hour % 12, "{:02d}".format(minute), 'am' if hour < 12 else 'pm')
+    print(f"time: {time} voltage: {voltage}v")
+    
+    if (hour > 17 or hour < 5):
         connect_relay()
+        print("grid lockin")
+    elif (voltage > 14.4):
+        disconnect_relay()
+        print("voltage high")
+    eliif (voltage < 12.1):
+        connect_relay()
+        print("voltage low")
