ae136bf597291b94bc8fd69b4b359aa7fd20c469
thewestlabs <111796612+thewestlabs@users.noreply.github.com>
2022-08-25 19:21:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import time
-from utils import read_voltage, disconnect_relay, connect_relay, read_time
+from utils import read_voltage, disconnect_relay, connect_relay, read_time, toggle_onboard_led
 
 while True:
     time.sleep(2)
+    toggle_onboard_led()
     voltage = read_voltage()
     year, month, date, hour, minute, second = read_time()
 
-    time = "{}:{}{}".format(hour % 12, "{:02d}".format(minute), 'am' if hour < 12 else 'pm')
-    print(f"time: {time} voltage: {voltage}v")
+    formated_time = "{}:{}{}".format(hour % 12, "{:02d}".format(minute), 'am' if hour < 12 else 'pm')
+    print(f"time: {formated_time} voltage: {voltage}v")
     
     if (hour > 17 or hour < 5):
         connect_relay()
